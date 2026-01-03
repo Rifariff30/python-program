ec483ef7c92020f7485fd8a2f5c4830b28036797
RIFA FATHIMA K P <rifafathima610@gmail.com>
2026-01-03 23:00:52 +0400

    updated

--- program_14/perfectsquare.py
+++ program_14/perfectsquare.py
@@ -1,7 +1,10 @@
+start = int(input("Enter start of range: "))
+end = int(input("Enter end of range: "))
 result = []
 for num in range(1000, 10000):
     digits = str(num)
     if all(int(d) % 2 == 0 for d in digits):
         if int(num ** 0.5) ** 2 == num:
             result.append(num)
+
 print(result)
